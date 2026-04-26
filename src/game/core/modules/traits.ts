10950e7831f5f2af2fd30b623485755f289b08e4
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-04-26 19:59:42 -0300

    fix constructor

--- src/game/core/modules/traits.ts
+++ src/game/core/modules/traits.ts
@@ -1,23 +1,15 @@
 import { Trait } from '../enums';
 
 export default class Traits {
-  values: Record<Trait, boolean>;
+  values: Trait[];
 
   constructor(
-    courious: boolean = false,
-    impulsive: boolean = false,
-    reckless: boolean = false,
-    obsessive: boolean = false,
+    has: Trait[]
   ) {
-    this.values = {
-      [Trait.Courious]: courious,
-      [Trait.Impulsive]: impulsive,
-      [Trait.Reckless]: reckless,
-      [Trait.Obsessive]: obsessive,
-    };
+    this.values = has;
   }
 
   has(trait: Trait) {
-    return this.values[trait];
+    return this.values.includes(trait);
   }
 }
